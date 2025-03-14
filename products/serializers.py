f1e2de714889b5da14ac1b6f738de465e6623500
agl5702 <advasquez41@misena.edu.co>
2025-03-14 17:47:46 -0500

    fix image problem

--- products/serializers.py
+++ products/serializers.py
@@ -7,6 +7,12 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+    def get_image(self, obj):
+        """Obtiene la URL correcta de Cloudinary."""
+        if obj.image:
+            return obj.image.url  # Usa la URL generada por Cloudinary
+        return None  # Si no hay imagen, devuelve None
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()  # Generar URL completa de la imagen
 
