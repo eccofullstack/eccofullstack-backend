c030b423c8a95c6df7cffbf5d6ba4780a24234a6
agl5702 <advasquez41@misena.edu.co>
2025-03-14 16:32:45 -0500

    fix image problem

--- products/views.py
+++ products/views.py
@@ -41,7 +41,32 @@ class CategoryViewSet(ViewSet):
 
 class ProductViewSet(ViewSet):
 
-    def list(self,request):
+    def list(self, request):
         queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response({"Message": "Producto eliminado correctamente"}, status=status.HTTP_204_NO_CONTENT)
